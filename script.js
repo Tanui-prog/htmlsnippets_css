58c5bd247e46bdb9b1341a06f37f5182f83d06b7
tanui-prog <vikiikiptoo@gmail.com>
2023-12-12 15:32:10 +0300

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 // menu icon
-
 let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('navbar');
+let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
-}
+    navbar.classList.toggle('active');
+};
+
+// Close the menu when a link is clicked
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', () => {
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+    });
+});
