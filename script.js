f33a6338e61fc1037e44695a24cb1f629a948714
tanui-prog <vikiikiptoo@gmail.com>
2023-12-12 14:36:01 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// menu icon
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+}
